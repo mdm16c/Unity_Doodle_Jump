02a0754f6dcc9b7111ee412ba13ad9d7bed26c43
Matthew McCracken <mdm16c@acu.edu>
2021-03-23 17:24:59 -0500

    bg move

---
+++ Assets/Scripts/moveBG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveBG : MonoBehaviour
+{
+    public Transform centerBackground;
+    public float delta = 10.0f;
+    void Update()
+    {
+        if (transform.position.y >= centerBackground.position.y + delta) {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y + delta);
+        }
+        else if (transform.position.y <= centerBackground.position.y - delta) {
+            centerBackground.position = new Vector2(centerBackground.position.x, transform.position.y - delta);
+        }
+    }
+}
