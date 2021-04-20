bc9d98056f263faa0419dc3ef1005940e4c50da2
Matthew McCracken <mdm16c@acu.edu>
2021-04-20 17:23:23 -0500

    fixed the platofrm spawning again

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -23,8 +23,8 @@ public class Destroy : MonoBehaviour
         float minY = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z)).y;
         float maxX = mainCam.ScreenToWorldPoint(new Vector3(0, 0, cam.transform.position.z)).x;
         float minX = mainCam.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, cam.transform.position.z)).x;
-        screenHeight = maxY - minY;
-        screenWidth = (maxX - minX)/2;
+        screenHeight = maxY - minY - .1f;
+        screenWidth = (maxX - minX - 3.0f)/2;
     }
 
     public void createStartingPlats() {
@@ -40,22 +40,22 @@ public class Destroy : MonoBehaviour
     {
         if (collision.gameObject.tag == "Platform") {
             if (Random.Range(1, 15) == 1) {
-                myEnemy = (GameObject)Instantiate(enemyPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (screenHeight + Random.Range(0.5f, 0.8f))), Quaternion.identity);
+                myEnemy = (GameObject)Instantiate(enemyPrefab, new Vector2(Random.Range(-screenWidth, screenWidth), player.transform.position.y + (screenHeight + Random.Range(0.5f, 0.8f))), Quaternion.identity);
                 myEnemy.SetActive(true);
             }
             if (Random.Range(1, 15) == 1) {
-                myPowerUp = (GameObject)Instantiate(powerUpPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (screenHeight + Random.Range(0.5f, 0.8f))), Quaternion.identity);
+                myPowerUp = (GameObject)Instantiate(powerUpPrefab, new Vector2(Random.Range(-screenWidth, screenWidth), player.transform.position.y + (screenHeight + Random.Range(0.5f, 0.8f))), Quaternion.identity);
                 myPowerUp.SetActive(true);
             }
 
             if (Random.Range(1, 10) == 1) {
-                myPlat = (GameObject)Instantiate(springPrefab, new Vector2(Random.Range(-5.5f, 5.5f), collision.transform.position.y + (screenHeight)), Quaternion.identity);
+                myPlat = (GameObject)Instantiate(springPrefab, new Vector2(Random.Range(-screenWidth, screenWidth), collision.transform.position.y + (screenHeight)), Quaternion.identity);
             }
             else if (Random.Range(1, 10) == 1) {
-                myPlat = (GameObject)Instantiate(breakPrefab, new Vector2(Random.Range(-5.5f, 5.5f), collision.transform.position.y + (screenHeight)), Quaternion.identity);
+                myPlat = (GameObject)Instantiate(breakPrefab, new Vector2(Random.Range(-screenWidth, screenWidth), collision.transform.position.y + (screenHeight)), Quaternion.identity);
             }
             else {
-                myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), collision.transform.position.y + (screenHeight)), Quaternion.identity);
+                myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-screenWidth, screenWidth), collision.transform.position.y + (screenHeight)), Quaternion.identity);
             }
         }
         Destroy(collision.gameObject);
