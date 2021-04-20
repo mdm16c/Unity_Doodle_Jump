5737113bbdddeb0072d236b11b16afe62a8b8532
ckk16a <ckk16a@acu.edu>
2021-04-20 16:29:35 -0500

    Scaling things

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Camera camera;
+    public float orthographicSize = 5;
+    public float aspect = 1.33333f;
+    void Start()
+    {
+        camera = this.gameObject.GetComponent<Camera>();
+        Camera.main.projectionMatrix = Matrix4x4.Ortho(
+                -orthographicSize * aspect, orthographicSize * aspect,
+                -orthographicSize, orthographicSize,
+                camera.nearClipPlane, camera.farClipPlane);
+    }
+}
